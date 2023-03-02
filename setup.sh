54064c4ef813dd136d758b5d7838b7f8cd4ac1ce
AntiLeaf <antileaf@163.com>
2023-03-02 21:07:06 +0800

    3.2 test for setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,26 @@
-pip install python3-venv
-python3 -m venv .venv
+#!/bin/bash
 
-source .venv/bin/activate
+# Check if python3-venv package is installed
+if ! dpkg -s python3-venv >/dev/null 2>&1; then
+    echo >&2 "python3-venv is not installed. Aborting."
+    exit 1
+fi
 
-pip install -r requirements.txt
+# Define the name of the virtual environment
+CIRNOBOT_VENV_NAME=.venv
+
+# Check if the virtual environment already exists
+if [ -d "$CIRNOBOT_VENV_NAME" ]; then
+    echo "Virtual environment already exists."
+	source "$CIRNOBOT_VENV_NAME/bin/activate"
+	
+else
+    # Create a new virtual environment
+    echo "Creating new virtual environment..."
+    python3 -m venv "$CIRNOBOT_VENV_NAME"
+
+	source "$CIRNOBOT_VENV_NAME/bin/activate"
+	# Install any required packages
+	pip install -r requirements.txt
+	
+fi
